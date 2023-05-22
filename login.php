4c1d8f8ceaef7da03fd7d247c2eb46c22f48233f
Ode Ardika <dirfanardika@gmail.com>
2023-05-22 10:46:12 +0800

    Menghubungkan dengan css di derectory html

--- login.php
+++ login.php
@@ -5,6 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link rel="stylesheet" href="html/css/footer.css">
+    <link rel="stylesheet" href="html/css/navbar.css">
+    <link rel="stylesheet" href="html/css/root.css">
 </head>
 <body>
     <form>
