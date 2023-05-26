de5e2269767e65e7a5324dbea5cf20f03eb777aa
Ode Ardika <dirfanardika@gmail.com>
2023-05-26 20:33:49 +0800

    Menyelesaikan desain dari wisata_page

--- wisata_page.php
+++ wisata_page.php
@@ -1,7 +1,21 @@
 <?php
-    require "db.php";
+    //Kirim Data kesini masuk
+    //$place_name = $_POST["place_name"];
+    //sementara
     $place_name = $_GET["place"];
-    $detail = get_detail_wisata($place_name);
+    $data = [
+        "GWK" => [
+            "title" => "Garuda Wisnu Kencana",
+            "address" => "Jl. Raya Uluwatu, Ungasan, Kuta Sel, Kabupaten Badung, Bali",
+            "fee" => "300.000"
+        ],
+        "Tanah Lot" => [
+            "title" => "Pantai Tanah Lot",
+            "address" => "Br. Beraban, Kec. Kediri, Kabupaten Tabanan, Bali",
+            "fee" => "200.000"
+        ],
+    ];
+    $selected_data = $data[$place_name]; 
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -10,22 +24,168 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo "$place_name"?> | Page</title>
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD"
+      crossorigin="anonymous"
+    />
+    <link rel="icon" href="assets/Logo.png" />
+    <link rel="stylesheet" href="assets/css/wisata.css" />
+    <link rel="stylesheet" href="assets/css/root.css" />
+    <link rel="stylesheet" href="assets/css/footer.css" />
+    <link rel="stylesheet" href="assets/css/navbar.css" />
+    <link
+      href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css"
+      rel="stylesheet"
+    />
 </head>
-<body id="nilai" getNilai="<?php echo $place_name;?>">
-    <img src="<?php echo "Tempat Wisata\\" . $place_name . "\\" . $place_name . "-1.jpg"; ?>" alt="">
-    <h1><?php echo $detail["title"]?></h1>
-    <h3>Alamat :</h3>
-    <h5><?php echo $detail["address"] ?></h5>
-    <h3>Harga Ticket : Rp <?php echo $detail["price"] ?></h3>
-    <label id="event">Events</label>
-    <div id="list event"></div><br>
-    <label id="review">Reviews</label>
-    <div id="list review"></div>
-    <h3>Foto - Foto Tempat Wisatanya :</h3>
-    <?php for($i = 1; $i <= 10; $i++): ?>
-        <img src="<?php echo "Tempat Wisata\\" . $place_name . "\\" . $place_name . "-$i.jpg"; ?>" alt="error" width="25%">
-    <?php endfor ?>
+<body>
+    <nav class="nav container-fluid" style="z-index: 1;">
+      <img src="assets/image/Brand.png" alt="" />
+      <div class="nav-items">
+        <a class="" href="index.html">Home</a>
+        <a class="" href="search.php">Search</a>
+        <a class="" href="">Top Wisata</a>
+        <div class="dropdown">
+          <button
+            class="btn-dropdown dropdown-toggle"
+            type="button"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            Help
+          </button>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">FAQ</a></li>
+            <li><a class="dropdown-item" href="#">Contact Us</a></li>
+            <li><a class="dropdown-item" href="#">About</a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="button-stack">
+        <a href="regis.php"><button class="btn-solid">Sign Up</button></a>
+        <a href="login.html"><button class="btn-outline">Login</button></a>
+      </div>
+    </nav>
+    <section class="hero">
+        <img class="bg-img" src="<?php echo "Tempat Wisata\\" . $place_name . "\\" . $place_name . "-1.jpg"; ?>" alt="">
+        <div class="copy">
+            <h1 class="header"><?php echo $selected_data["title"]?></h1>
+            <h1 class="address"><?php echo $selected_data["address"] ?></h1> 
+        </div>
+    </section>
+    
+    <section class="content">
+        <section class="left-content">
+            <section class="info">
+                <h1 class="title"><?php echo $selected_data["title"]?></h1>
+                <h1 class="desc">Dictumst scelerisque ut commodo dis. Risus ac tellus sapien gravida sit elementum dui eget nunc. Eu arcu montes, sit elit, maecenas feugiat. Urna, habitant suspendisse suspendisse pharetra nec. Nibh mauris nullam nec mattis.</h1>
+                <h1 class="fee">Fee: <?php echo $selected_data["fee"] ?></h1>
+            </section>
 
+            <section class="gallery">
+                <h1 class="title">Gallery</h1>
+                <div id="carousel" class="carousel slide" data-bs-ride="carousel">
+                    <div class="carousel-indicators">
+                        <button type="button" data-bs-target="#carousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                        <button type="button" data-bs-target="#carousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                        <button type="button" data-bs-target="#carousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                    </div>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                        <img src="<?php echo "Tempat Wisata\\" . $place_name . "\\" . $place_name . "-2.jpg"; ?>" class="d-block w-100" alt="unavailable">
+                        </div>
+                        <div class="carousel-item">
+                        <img src="<?php echo "Tempat Wisata\\" . $place_name . "\\" . $place_name . "-3.jpg"; ?>" class="d-block w-100" alt="unavailable">
+                        </div>
+                        <div class="carousel-item">
+                        <img src="<?php echo "Tempat Wisata\\" . $place_name . "\\" . $place_name . "-4.jpg"; ?>" class="d-block w-100" alt="unavailable">
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carousel" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carousel" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                    </div>
+            </section>
+        </section>
+        <div class="divider"></div>
+        <section class="right-content">
+            <section class="event">
+                <h1 class="title">Events</h1>
+                <div id="list-event"><!-- nanti list eventnya disini --></div>
+            </section>
+            <section class="reviews">
+                    <h1 class="title">Reviews</h1>
+                    <div id="list-review"><!-- nanti list reviewnya disini --></div>
+            </section>
+        </section>
+    </section>
+
+    
+
+    <footer class="footer container-fluid">
+      <div id="connect">
+        Connect With Us
+        <div class="social-icon">
+          <a href="linked.com"
+            ><img src="assets/image/linkedin.png" alt=""
+          /></a>
+          <a href="facebook.com"><img src="assets/image/fb.png" alt="" /></a>
+          <a href="twitter.com"><img src="assets/image/twt.png" alt="" /></a>
+        </div>
+      </div>
+      <div id="explore">
+        Explore
+        <ul class="navigation">
+          <li>
+            <a href="#">Top Wisata</a>
+          </li>
+          <li>
+            <a href="#">Navigation 2</a>
+          </li>
+          <li>
+            <a href="#">Navigation 3</a>
+          </li>
+        </ul>
+      </div>
+
+      <div id="support">
+        Support
+        <ul class="navigation">
+          <li>
+            <a href="#">FAQ</a>
+          </li>
+          <li>
+            <a href="#">Contact Us</a>
+          </li>
+          <li>
+            <a href="#">Visit Us</a>
+          </li>
+        </ul>
+      </div>
+
+      <div id="copyright">
+        <a href="index.html"><img src="assets/image/Brand.png" alt="" /></a>
+        © 2023 Wisata Find, All Rights Reserved.
+      </div>
+    </footer>
+    <script src="assets/js/navbar.js"></script>
     <script src="js\function.js"></script>
+    <script
+      src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js"
+      integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE"
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js"
+      integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ"
+      crossorigin="anonymous"
+    ></script>
 </body>
-</html>
+</html>    
